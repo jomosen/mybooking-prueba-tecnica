16407aca1cbaa670f58809aec297388bdf43e95a
jomosen <josemorellsendra@gmail.com>
2025-08-27 16:27:18 +0200

    add endpoint, service and use case to list seasons for season definition

--- app/controller/api/prices_controller.rb
+++ app/controller/api/prices_controller.rb
@@ -36,6 +36,22 @@ module Controller
           end
         end
 
+        app.get '/api/season-definitions/:season_definition_id/seasons' do
+
+          service = Service::ListSeasonsForSeasonDefinitionService.new
+          use_case = UseCase::ListSeasonsForSeasonDefinitionServiceUseCase.new(service, logger)
+          result = use_case.perform(params)
+
+          if result.success?
+            content_type :json
+            result.data.to_json
+          elsif !result.authorized?
+            halt 401
+          else
+            halt 400, result.message.to_json
+          end
+        end
+
       end
 
     end
