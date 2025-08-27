d2b0c4cc62a29414989618f3151b4dfef37bdee2
jomosen <josemorellsendra@gmail.com>
2025-08-27 16:25:41 +0200

    fix bug query run with varargs instead of array and remove order by

--- app/service/list_season_definitions_for_rate_type_service.rb
+++ app/service/list_season_definitions_for_rate_type_service.rb
@@ -9,11 +9,10 @@ module Service
         JOIN season_definition_rental_locations sdrl ON sd.id = sdrl.season_definition_id
         JOIN category_rental_location_rate_types crlrt ON crlrt.rental_location_id = sdrl.rental_location_id
         JOIN rate_types rt ON rt.id = crlrt.rate_type_id
-        WHERE crlrt.rate_type_id = ? AND crlrt.rental_location_id = ?
-        ORDER BY sd.name ASC
+        WHERE crlrt.rental_location_id = ? AND crlrt.rate_type_id = ?
       SQL
 
-      Infraestructure::Query.run(sql, [params[:rate_type_id], params[:rental_location_id]])
+      Infraestructure::Query.run(sql, params[:rental_location_id], params[:rate_type_id])
 
     end
   end
