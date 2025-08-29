cf0dcce2ba513e504bc481ce5dcce1153b2ce9f1
jomosen <josemorellsendra@gmail.com>
2025-08-29 16:06:21 +0200

    add upsert method in price repository

--- app/repository/price_repository.rb
+++ app/repository/price_repository.rb
@@ -1,7 +1,38 @@
 module Repository
   class PriceRepository < Repository::BaseRepository
+
     def initialize
       super(Model::Price)
     end
+
+    # Con upsert nos ahorramos tener que traer primero el precio: mejoramos rendimiento y atomicidad (lo hacemos en una sola operació atómica)
+    def upsert(command)
+      begin
+
+        sql = <<-SQL
+          INSERT INTO prices
+            (price_definition_id, season_id, time_measurement, units, price, included_km, extra_km_price)
+          VALUES
+            (?, ?, ?, ?, ?, ?, ?)
+          ON DUPLICATE KEY UPDATE
+            price         = VALUES(price),
+            included_km   = VALUES(included_km),
+            extra_km_price= VALUES(extra_km_price)
+        SQL
+
+        DataMapper.repository(:default).adapter.execute(sql, 
+          command[:price_definition_id],
+          command[:season_id],
+          command[:time_measurement],
+          command[:units],
+          command[:price],
+          command[:included_km],
+          command[:extra_km_price])
+
+      rescue => e
+        raise e
+      end
+    end
+
   end
 end
