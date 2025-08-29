11229b61748cdb388f7c6bdd78f04356cdf94c3e
jomosen <josemorellsendra@gmail.com>
2025-08-29 16:00:27 +0200

    remove unused code

--- app/controller/admin/home_controller.rb
+++ app/controller/admin/home_controller.rb
@@ -4,16 +4,12 @@ module Controller
 
       def self.registered(app)
 
-        #
-        # Home page
-        #
         app.get '/' do
 
           rental_location_repository = Repository::RentalLocationRepository.new
           use_case = PageUseCase::Home::PageHomeUseCase.new(rental_location_repository, logger)
           result = use_case.perform(params)
 
-          @title = "Home page"
           @rental_locations = result.data
 
           if result.success?
